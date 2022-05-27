28b4d89e8251ef4bb8fc625e2cbc4534c44b3915
unknown <georgeclinton.1792@gmail.com>
2022-05-28 00:19:52 +0530

    Creating Expo Project, adding stack navigation, Designing  Home Component

---
+++ NFT/components/index.js
@@ -0,0 +1,5 @@
+import Status_Bar from "./focused-statusbar";
+import Nft_card from "./nft-card";
+import Home_Header from "./home-header";
+
+export { Status_Bar, Nft_card, Home_Header };
---
+++ NFT/components/nft-card.js
@@ -0,0 +1,10 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+export default function Nft_card() {
+  return (
+    <View>
+      <Text>Nft_card</Text>
+    </View>
+  )
+}
